9e9aff85d22d14da982a05b57e44f6862fce5f27
armero98 <armerosteven@gmail.com>
2021-06-23 20:01:46 -0500

    se creó un componente "operacion"

--- calculadoraElectiva/src/app/app.module.ts
+++ calculadoraElectiva/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { OperacionComponent } from './operacion/operacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OperacionComponent
   ],
   imports: [
     BrowserModule
